210fa33f67c0489afa61b5422d2f37e71950113b
maxamedcalicism12-lang <cxc5126@gmail.com>
2026-09-14 00:41:25 -0700

    chore: remove node_modules, secrets, and build artifacts from git tracking

--- laas-real-estate-react/backend/src/helpers/activityLog.js
+++
@@ -1,9 +0,0 @@
-const db = require('../config/db');
-
-function logActivity(userId, action, module, description) {
-  db.prepare(
-    'INSERT INTO activity_logs (user_id, action, module, description, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)'
-  ).run(userId || null, action, module, description, db.now(), db.now());
-}
-
-module.exports = { logActivity };
